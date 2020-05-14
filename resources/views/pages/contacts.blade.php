20f950f20b00bf5d0654544fbd306d9ba667315f
Andrew Earls <andrewearls@rocketmail.com>
2020-05-14 13:23:41 -0700

    contacts form

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-Contacts
+<div class="container-fluid">
+	<div class="row justify-content-center">
+		<div class="col-lg-6">
+			<div class="card">
+				<div class="card-header">
+					Contacts
+				</div>
+				<div class="card-body">
+					<form action="{{route('contacts')}}" method="post">
+						@csrf
+						<div class="row justify-content-center">
+							<div class="col-lg-3">
+								<label>First Name</label><br>
+								<label>Email</label><br>
+								<label>Phone</label><br>
+							</div>
+							<div class="col-lg-3">
+								<input type="text" name="first_name">
+								<input type="email" name="email">
+								<input type="text" name="phone">
+							</div>
+						</div>
+						<div class="row justify-content-center">
+							<div class="col-lg-3">
+								<input type="submit" name="submit">
+							</div>
+						</div>
+						
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</div>
 @endsection
