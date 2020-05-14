03cd081abdedd82b00090d81a946c10fe754d8ea
Andrew Earls <andrewearls@rocketmail.com>
2020-05-14 13:41:20 -0700

    contact form validator

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactFormValidator;
 
 class ContactsController extends Controller
 {
     public function index()
     {
     	return view('pages.contacts');
+    }
+
+    /*
+     * Persist new contact to database
+     */
+    public function persist(ContactFormValidator $request)
+    {
+    	$validated = $request->validated();
+    	return $validated;
     }
 }
---
+++ app/Http/Requests/ContactFormValidator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactFormValidator extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        //user has already been authorized in routes
+        return ture;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|alpha_num',
+            'email' => 'required|email:rfc',
+            'phone' => 'required|numeric',
+        ];
+    }
+}
