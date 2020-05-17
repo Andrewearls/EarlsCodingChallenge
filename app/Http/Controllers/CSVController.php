b83f1ad72d466bfe2d636969666be9f843155f38
Andrew Earls <andrewearls@rocketmail.com>
2020-05-17 10:42:34 -0700

    csv import controller

--- app/Http/Controllers/CSVController.php
+++ app/Http/Controllers/CSVController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class CSVController extends Controller
 {
-    //
+    public function index()
+    {
+    	return view('pages.csvImport');
+    }
+
+    public function persist(Request $request)
+    {
+    	return $request;
+    }
 }
